601c6af90b60f037f764f039052dc8b473176c2a
Sherifzade-Bajrami <sb51075@ubt-uni.net>
2023-02-08 21:48:16 +0100

    AboutModel

---
+++ AlumniAssociationF/Models/About.cs
@@ -0,0 +1,14 @@
+﻿namespace AlumniAssociationF.Models
+{
+    public class About
+    {
+        public int Id { get; set; }
+
+        public string? Image { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+       
+    }
+}
