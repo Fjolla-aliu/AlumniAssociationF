ac7564b2fb01a953253b449c5a12557928b4780b
ervin osmani <eo51073@ubt-uni.net>
2023-02-08 21:54:05 +0100

    Center model

---
+++ AlumniAssociationF/Models/Center.cs
@@ -0,0 +1,12 @@
+﻿namespace AlumniAssociationF.Models
+{
+    public class Center
+    {
+        public int Id { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+    }
+
+}
