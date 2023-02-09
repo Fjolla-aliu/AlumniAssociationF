33ddd0a4af2f720a70aae5896a5b6f89009b60ff
Sherifzade-Bajrami <sb51075@ubt-uni.net>
2023-02-09 21:11:23 +0100

    AboutsAPIController

---
+++ AlumniAssociationF/Controllers/AboutsAPIController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AlumniAssociationF.Data;
+using AlumniAssociationF.Models;
+
+namespace AlumniAssociationF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutsAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AboutsAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AboutsAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<About>>> GetAbout()
+        {
+            return await _context.About.ToListAsync();
+        }
+
+        // GET: api/AboutsAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<About>> GetAbout(int id)
+        {
+            var about = await _context.About.FindAsync(id);
+
+            if (about == null)
+            {
+                return NotFound();
+            }
+
+            return about;
+        }
+
+        // PUT: api/AboutsAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAbout(int id, About about)
+        {
+            if (id != about.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(about).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AboutExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AboutsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<About>> PostAbout(About about)
+        {
+            _context.About.Add(about);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAbout", new { id = about.Id }, about);
+        }
+
+        // DELETE: api/AboutsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAbout(int id)
+        {
+            var about = await _context.About.FindAsync(id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+
+            _context.About.Remove(about);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AboutExists(int id)
+        {
+            return _context.About.Any(e => e.Id == id);
+        }
+    }
+}
